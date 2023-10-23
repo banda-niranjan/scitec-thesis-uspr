3d523234a650ced4afb48b5455d60c38ec3c9b95
Niranjan <niranjan.banda@stud.eah-jena.de>
2023-10-23 10:01:36 +0200

    #Comments - Links

--- DataViz/ds2302a.py
+++ DataViz/ds2302a.py
@@ -1,5 +1,9 @@
 import pyvisa
 
+#https://www.righto.com/2013/07/rigol-oscilloscope-hacks-with-python.html
+#https://www.tek.com/en/documents/technical-brief/getting-started-with-oscilloscope-automation-and-python
+
+
 instrument= pyvisa.ResourceManager().open_resource("TCPIP::192.168.1.141::INSTR")
 instrument.query(":MEASure:FREQuency:SMAXimum")
 
