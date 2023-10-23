1b1d32cffbaf976c076728975150cbd150b94c69
banda-niranjan <niranjan.banda@stud.eah-jena.de>
2023-10-23 14:52:05 +0200

    Working Oscii pyvisa queries

--- DataViz/ds2302a.py
+++ DataViz/ds2302a.py
@@ -1,11 +1,12 @@
 import pyvisa
+import pandas as pd
 
-#https://www.righto.com/2013/07/rigol-oscilloscope-hacks-with-python.html
-#https://www.tek.com/en/documents/technical-brief/getting-started-with-oscilloscope-automation-and-python
+resource_manager = pyvisa.ResourceManager()
+# You can change the variable name and resource name
 
+oscii = resource_manager.open_resource("USB0::0x1AB1::0x04B0::DS2D244503546::INSTR")
 
-instrument= pyvisa.ResourceManager().open_resource("TCPIP::192.168.1.141::INSTR")
-instrument.query(":MEASure:FREQuency:SMAXimum")
-
+print(oscii)
+print(oscii.query("*IDN?"))
 
 
