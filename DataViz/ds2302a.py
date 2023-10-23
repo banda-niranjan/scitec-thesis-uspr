6bc9352c1d67c7c95fc0205e16b41378ae669bff
banda-niranjan <niranjan.banda@stud.eah-jena.de>
2023-10-23 14:55:08 +0200

    Added pyvisa and other backend libraries

--- DataViz/ds2302a.py
+++ DataViz/ds2302a.py
@@ -2,7 +2,7 @@ import pyvisa
 import pandas as pd
 
 resource_manager = pyvisa.ResourceManager()
-# You can change the variable name and resource name
+# You can change the variable names and resource name
 
 oscii = resource_manager.open_resource("USB0::0x1AB1::0x04B0::DS2D244503546::INSTR")
 
